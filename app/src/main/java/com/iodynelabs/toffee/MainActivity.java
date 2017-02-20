393967a42d2027984dc28d2341c8d50c2eedc606
Unknown <ruizalejandro52@gmail.com>
2017-02-20 12:11:10 -0500

    Create Project

---
+++ app/src/main/java/com/iodynelabs/toffee/MainActivity.java
@@ -0,0 +1,13 @@
+package com.iodynelabs.toffee;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
